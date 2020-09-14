b7c141c025c0819fdeeba7edf89a692a22b7f4fa
LXie-Saintex <admin@saintex.co.uk>
2020-09-14 07:24:32 +0100

    symbology

--- src/basic.js
+++ src/basic.js
@@ -1,8 +1,12 @@
-import { Symbology } from './symbology'; 
+const Symbology = {
+    work : " 💰 ",
+    interest : " 👀 ",
+    have_to_do : " 🙄 ",
+    asap: " 🚨 ",
+    brain_food: " 🧠 "
+}
 
 function addLeftoversSymbols (){
-    console.log(Symbology.work);
-
     const items = document.querySelectorAll('.leftovers li');
     for(let item of items){
         let itemClasses = item.getAttribute("class");
@@ -26,6 +30,18 @@ function addLeftoversSymbols (){
     }
 }
 
+function explainSymbols() {
+    const para = document.createElement('p');
+    para.textContent = `Some items are prefixed with emojis, which means that the item in question has higher priority due to \
+    a variety of reasons; ${Symbology.work} means that it is something that would help me at work; I prefix the items that I'm particularly \
+    interested in with ${Symbology.interest}, and the ones that I consider good for me with ${Symbology.brain_food}; \
+    For the items that I couldn't care less about but are required somehow, I prefix them with ${Symbology.have_to_do}; \
+    if an item is prefixed ${Symbology.asap}, it means it should be done ASAP.`;
+    const itemsList= document.querySelector('.leftovers');
+    document.body.insertBefore(para,itemsList);
+}
+
 window.onload = function() {
-    addLeftoversSymbols(); 
+    addLeftoversSymbols();
+    explainSymbols();
 }
